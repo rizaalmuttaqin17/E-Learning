dbad3d117f56541b8a37a3381305d5f5312798f8
rizaalmuttaqin17 <rizabreakerz@gmail.com>
2022-12-10 16:19:20 +0800

    update

--- resources/views/program_studis/fields.blade.php
+++ resources/views/program_studis/fields.blade.php
@@ -1,9 +1,3 @@
-<!-- Id Fakultas Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('id_fakultas', __('models/programStudis.fields.id_fakultas').':') !!}
-    {!! Form::number('id_fakultas', null, ['class' => 'form-control']) !!}
-</div>
-
 <!-- Kode Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('kode', __('models/programStudis.fields.kode').':') !!}
--- resources/views/program_studis/table.blade.php
+++ resources/views/program_studis/table.blade.php
@@ -2,28 +2,27 @@
     <table class="table" id="programStudis-table">
         <thead>
             <tr>
-                <th>@lang('models/programStudis.fields.id_fakultas')</th>
-        <th>@lang('models/programStudis.fields.kode')</th>
-        <th>@lang('models/programStudis.fields.nama')</th>
-                <th colspan="3">@lang('crud.action')</th>
+                <th>No.</th>
+                <th>@lang('models/programStudis.fields.kode')</th>
+                <th>@lang('models/programStudis.fields.nama')</th>
+                <th colspan="3">Action</th>
             </tr>
         </thead>
         <tbody>
         @foreach($programStudis as $programStudi)
             <tr>
-                       <td>{{ $programStudi->id_fakultas }}</td>
-            <td>{{ $programStudi->kode }}</td>
-            <td>{{ $programStudi->nama }}</td>
-                       <td class=" text-center">
-                           {!! Form::open(['route' => ['programStudis.destroy', $programStudi->id], 'method' => 'delete']) !!}
-                           <div class='btn-group'>
-                               <a href="{!! route('programStudis.show', [$programStudi->id]) !!}" class='btn btn-light action-btn '><i class="fa fa-eye"></i></a>
-                               <a href="{!! route('programStudis.edit', [$programStudi->id]) !!}" class='btn btn-warning action-btn edit-btn'><i class="fa fa-edit"></i></a>
-                               {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger action-btn delete-btn', 'onclick' => 'return confirm("'.__('crud.are_you_sure').'")']) !!}
-                           </div>
-                           {!! Form::close() !!}
-                       </td>
-                   </tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $programStudi->kode }}</td>
+                <td>{{ $programStudi->nama }}</td>
+                <td class=" text-center">
+                    {!! Form::open(['route' => ['programStudis.destroy', $programStudi->id], 'method' => 'delete']) !!}
+                    <div class='btn-group'>
+                        <a href="{!! route('programStudis.show', [$programStudi->id]) !!}" class='btn btn-light action-btn '><i class="fa fa-eye"></i></a>
+                        <a href="{!! route('programStudis.edit', [$programStudi->id]) !!}" class='btn btn-warning action-btn edit-btn'><i class="fa fa-edit"></i></a>
+                        {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger action-btn delete-btn', 'onclick' => 'return confirm("'.__('crud.are_you_sure').'")']) !!}
+                    </div>
+                    {!! Form::close() !!}
+                </td>
             </tr>
         @endforeach
         </tbody>
