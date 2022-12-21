48ba8bc08d3127827e01748016f4d504cc9d26ec
rizaalmuttaqin17 <rizabreakerz@gmail.com>
2022-12-21 17:30:13 +0800

    update

--- resources/views/ujians/selesai.blade.php
+++ resources/views/ujians/selesai.blade.php
@@ -1,1 +1,6 @@
-<input data-id="{{ $id }}" class="toggle-class" data-toggle="toggle" data-on="true" data-off="false" data-onstyle="warning" data-offstyle="dark" type="checkbox" {{ $selesai == 'true' ? 'checked' : '' }}>
+{{-- <input data-id="{{ $id }}" class="toggle-class" data-toggle="toggle" data-on="true" data-off="false" data-onstyle="warning" data-offstyle="dark" type="checkbox" {{ $selesai == 'true' ? 'checked' : '' }}> --}}
+
+<label class="custom-switch">
+    <input type="radio" name="option" value="{{ $selesai }}" class="custom-switch-input" checked="">
+    <span class="custom-switch-indicator"></span>
+</label>
