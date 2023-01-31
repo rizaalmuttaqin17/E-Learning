30f7009f9efc9242aab87436c995c7ce0d40e0d7
rizaalmuttaqin <rizabreakerz@gmail.com>
2023-01-31 08:56:29 +0800

    update

--- resources/views/ujians/table_nilai_actions.blade.php
+++ resources/views/ujians/table_nilai_actions.blade.php
@@ -1,2 +1,5 @@
-<a href="" class="btn btn-outline-primary btn-lg btn-block" role="button" aria-pressed="true" data-target="#isiNilai" data-toggle="modal">Isi Nilai</a>
+@if ($model['soals']['id_tipe_soal'] == 1)
+@else
+<a href="" class="btn btn-outline-primary btn-lg btn-block" role="button" aria-pressed="true" data-target="#isiNilai{{ $model['id'] }}" data-toggle="modal">Isi Nilai</a>
 @include('ujians.modals.nilai_soal')
+@endif
